d6cbba8b8031af3ca4d165bf5a42de92f6b3843d
Tate Keller <tatek1993@gmail.com>
2020-02-18 22:56:11 -0600

    Began setting up connects

--- src/reducers/carSaleReducer.js
+++ src/reducers/carSaleReducer.js
@@ -1,31 +1,39 @@
 
-export const carSaleReducer = (state = initialState, action) => {
+export const initialState = {
+    
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+};
+
+export const carSaleReducer = (state = initialState, action) => { console.log('stoot', state);
     switch (action.type) {
-        case 'ADD_ITEM' :
-            const newItem = {
-                name: action.payload.name,
-                price: action.payload.price
-            }
-            return {
-                ...state,
-                car: {...state.car, features: [...state.car.features, ...newItem] }
-            };
-
-
         case 'BUY_ITEM' :
             const boughtItem = {
                 name: action.payload.name,
                 price: action.payload.price
             }
+            console.log(state);
             return {
                 ...state,
                 car: {...state.car, features: [...state.car.features, ...boughtItem] }
             };
-
-        case 'REMOVE_FEATURE' :
-            return {
-                car: state.car.features.filter(item => ),
-                car: [ ...state.car, {features: [...state.car.features, ...newItem]}]
-            }
+        default: return state;
+        // case 'REMOVE_FEATURE' :
+        //     return {
+        //         // car: state.car.features.filter(item => ),
+        //         car: [ ...state.car, {features: [...state.car.features, ...newItem]}]
+        //     }
     } 
 }
